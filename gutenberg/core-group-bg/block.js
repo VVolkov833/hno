ed1f9e81bb6172f9dc7c7d2f217479a0b6fe6723
VVolkov833 <vadim.volkov.dev@gmail.com>
2023-07-22 09:24:41 +0200

    almost working classes toggling

--- gutenberg/core-group-bg/block.js
+++ gutenberg/core-group-bg/block.js
@@ -1,20 +1,20 @@
-( function() {
+(function () {
 
     const effected_blocks = ['core/group'];
-    
-    // add new settings variable
+
+    // add new settings variables
     wp.hooks.addFilter(
         'blocks.registerBlockType',
         blockName + '-data',
         function (settings, name) {
 
-            if ( typeof settings.attributes === 'undefined' || !~effected_blocks.indexOf( name ) ) { return settings }
+            if (typeof settings.attributes === 'undefined' || !~effected_blocks.indexOf(name)) { return settings }
 
-            settings.attributes = Object.assign( settings.attributes, {
-                curveTop: {
+            settings.attributes = Object.assign(settings.attributes, {
+                curve_top: {
                     type: 'boolean',
                 },
-                curveBottom: {
+                curve_bottom: {
                     type: 'boolean',
                 }
             });
@@ -26,32 +26,37 @@
     // add the control / input
     const el = wp.element.createElement;
     const toggle = (props, name, label) => {
-        return props.isSelected && ~effected_blocks.indexOf( props.name ) ? (
-            el( wp.blockEditor.InspectorControls, {},
-                el( wp.components.PanelBody, {},
-                    el( wp.components.ToggleControl, {
+        return ( props.isSelected && ~effected_blocks.indexOf(props.name) ) ? (
+            el(wp.blockEditor.InspectorControls, {},
+                el(wp.components.PanelBody, {},
+                    el(wp.components.ToggleControl, {
                         label: label,
                         checked: !!props.attributes[name],
-                        onChange: function() {
-                            console.log( name, props.attributes[name] );
-                            props.setAttributes( { [name]: !props.attributes[name] } );
+                        onChange: function () {
+                            //console.log( props.attributes.className );
+                            props.setAttributes({ [name]: !props.attributes[name], className: 'ahaha' });
+                            console.log( props.attributes.className );
+                            //props.setAttributes({ className: updatedClassName });
+                            //console.log( props.getAttribute( 'className' ) );
+                            //props.setAttributes({ 'data-ahaha': 'ahaha' });
                         }
                     })
                 )
             )
         ) : null
     };
+
     wp.hooks.addFilter(
         'editor.BlockEdit',
         blockName + '-control',
-        wp.compose.createHigherOrderComponent( function ( BlockEdit ) {
-            return function ( props ) {
+        wp.compose.createHigherOrderComponent(function (BlockEdit) {
+            return function (props) {
                 return el(
                     wp.element.Fragment,
                     {},
-                    el( BlockEdit, props ),
-                    toggle( props, 'curveTop', 'Apply Top Curve' ),
-                    toggle( props, 'curveBottom', 'Apply Bottom Curve' )
+                    el(BlockEdit, props),
+                    toggle(props, 'curve_top', 'Apply Top Curve'),
+                    toggle(props, 'curve_bottom', 'Apply Bottom Curve')
                 );
             };
         })
@@ -63,10 +68,32 @@
         blockName + '-save',
         function (extraProps, blockType, attributes) {
 
-            if ( !~effected_blocks.indexOf( blockType.name ) ) { return extraProps }
-            if ( typeof attributes.curveTop === 'undefined' || !attributes.curveTop ) { return extraProps }
+            if (!~effected_blocks.indexOf(blockType.name)) { return extraProps }
 
-            extraProps.className = extraProps.className + ' fct-img-to-bg';
+            const addClass = (classNames, classNameToAdd) => {
+                const classes = classNames.split(' ');
+                if (!classes.includes(classNameToAdd)) {
+                    classes.push(classNameToAdd);
+                }
+                return [...new Set(classes)].join(' ');
+            };
+
+            const removeClass = (classNames, classNameToRemove) => {
+                const classes = classNames.split(' ');
+                const index = classes.indexOf(classNameToRemove);
+                if (index !== -1) {
+                    classes.splice(index, 1);
+                }
+                return classes.join(' ');
+            };
+
+            if ( attributes?.curve_top ) {
+                extraProps.className = addClass(extraProps.className, 'fct-curve-top');
+            }
+            if ( attributes?.curve_bottom ) {
+                extraProps.className = addClass(extraProps.className, 'fct-curve-bottom');
+            }
+
             return extraProps;
         }
     );
